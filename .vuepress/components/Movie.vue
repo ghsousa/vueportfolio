6a3926ff125ac94c6ef6fe7afdda4acf73b94ccf
Gabriel Sousa <gsousa09@gmail.com>
2020-08-06 11:46:57 -0400

    Turn intro movie into a link

--- .vuepress/components/Movie.vue
+++ .vuepress/components/Movie.vue
@@ -1,10 +1,12 @@
 <template>
 
 <div class="movie-container">
+<router-link to="/about/">
 <video width="100%" height="100%" autoplay loop muted> 
   <source src="/upload/intro.mp4" type="video/mp4">
   Your browser does not support the video tag.
 </video>
+</router-link>
 </div>
 
 </template>
