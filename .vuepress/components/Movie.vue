2183cfaa0e94ff396402f63ed66e7ff1d1e819d6
Gabriel Sousa <gsousa09@gmail.com>
2020-12-06 09:16:24 -0700

    Replace intro movie with GIF

--- .vuepress/components/Movie.vue
+++ .vuepress/components/Movie.vue
@@ -2,10 +2,7 @@
 
 <div class="movie-container">
 <router-link to="/about/">
-<video width="100%" height="100%" autoplay loop muted> 
-  <source src="/upload/intro.mp4" type="video/mp4">
-  Your browser does not support the video tag.
-</video>
+<img  src="/upload/intro.gif" >
 </router-link>
 </div>
 
@@ -20,11 +17,12 @@ export default {
 <style scoped>
 .movie-container {
 	margin: auto;
-	max-width: 800px;
+	max-width: 600px;
 }
 
-video {
+img {
 	margin: auto;
-		display: block;
+	display: block;
+	
 }
 </style>
